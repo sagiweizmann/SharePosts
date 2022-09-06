16a2966adf9a06035ded3945fa838af524390ed1
sagiweizmann <sagiweizmann@gmail.com>
2022-09-06 10:23:12 +0300

    Latest Changes

--- protected/config/database.php
+++ protected/config/database.php
@@ -2,13 +2,10 @@
 
 // This is the database connection configuration.
 return array(
-	'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-	// uncomment the following lines to use a MySQL database
-	/*
-	'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+	//'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',	
+	'connectionString' => 'mysql:host=localhost;dbname=SharePosts',
 	'emulatePrepare' => true,
 	'username' => 'root',
-	'password' => '',
+	'password' => '77021002',
 	'charset' => 'utf8',
-	*/
 );
