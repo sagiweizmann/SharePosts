d428af140c06c075746309565a257b98c6870e73
sagiweizmann <sagiweizmann@gmail.com>
2022-09-18 20:58:35 +0300

    UserIdentitiy fix to check password correctly

--- protected/components/UserIdentity.php
+++ protected/components/UserIdentity.php
@@ -7,6 +7,7 @@
  */
 class UserIdentity extends CUserIdentity
 {
+	private $db_id;
 	/**
 	 * Authenticates a user.
 	 * The example implementation makes sure if the username and password
@@ -18,17 +19,21 @@ class UserIdentity extends CUserIdentity
 	public function authenticate()
 	{
 		$user = new Users('login');
-		$this->password=password_hash($this->password,PASSWORD_DEFAULT);
 		$user = $user->login($this->username);
+		$this->db_id = $user->id;
 		if(!isset($user->username)){
 			$this->errorCode=self::ERROR_USERNAME_INVALID;
 		}
-		elseif(password_verify($this->password, $user->password))
+		elseif(!password_verify($this->password, $user->password))
 		{
 			$this->errorCode=self::ERROR_PASSWORD_INVALID;		
 		}
 		else
 			$this->errorCode=self::ERROR_NONE;
 		return !$this->errorCode;
+	}
+	public function getId()
+	{
+    	return $this->db_id;
 	}
 }
