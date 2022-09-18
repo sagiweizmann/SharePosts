25fc5059e17a0d31d7b997597b632b4e0b417993
sagiweizmann <sagiweizmann@gmail.com>
2022-09-18 20:59:46 +0300

    Posts shown in index

--- themes/bootstrap/views/site/index.php
+++ themes/bootstrap/views/site/index.php
@@ -1,25 +1,40 @@
 <?php
 /* @var $this SiteController */
 
-$this->pageTitle=Yii::app()->name;
+$this->pageTitle = Yii::app()->name;
 ?>
 
-<?php $this->beginWidget('bootstrap.widgets.TbHeroUnit',array(
-    'heading'=>'Welcome to '.CHtml::encode(Yii::app()->name),
+<?php $this->beginWidget('bootstrap.widgets.TbHeroUnit', array(
+    'heading' => 'Welcome to ' . CHtml::encode(Yii::app()->name),
 )); ?>
+<?php
+if (Yii::app()->user->isGuest) : 
+?>
+    <div class="alert alert-block">
+        <p>Please login to see the all posts !</p>
+    </div>
+<?php
+else :
+?>
+    <?php
+    $posts = Post::getAllPosts();
+    foreach (array_reverse($posts) as $post) :
+    ?>
+        <div class="post">
+            <div class="post__user">
 
-<p>Congratulations! You have successfully created your Yii application.</p>
+                <strong class="post__username"><?php echo $post->title; ?></strong>
+                <span class="post__date"><?php echo $post->created_at; ?></span>
+
+                <div class="post__body">
+                    <?php echo $post->body; ?>
+                </div>
+                <?php echo 'Posted By ' . '<span style="color:grey;">'. Users::getUserNameById($post->user_id); '</span>' ?>
+            </div>
+        </div>
+    <?php
+    endforeach;
+    ?>
+<?php endif; ?>
 
 <?php $this->endWidget(); ?>
-
-<p>You may change the content of this page by modifying the following two files:</p>
-
-<ul>
-    <li>View file: <code><?php echo __FILE__; ?></code></li>
-    <li>Layout file: <code><?php echo $this->getLayoutFile('main'); ?></code></li>
-</ul>
-
-<p>For more details on how to further develop this application, please read
-    the <a href="http://www.yiiframework.com/doc/">documentation</a>.
-    Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
-    should you have any questions.</p>
